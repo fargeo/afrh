b4fc4c64c2ecd50580f8eef342823f47379ce82b
Galen Mancino <whatisgalen@gmail.com>
2020-06-22 15:34:16 -0700

    renames properties for table view

--- afrh_prj/media/js/views/components/plugins/active-consultations.js
+++ afrh_prj/media/js/views/components/plugins/active-consultations.js
@@ -243,11 +243,11 @@ define([
                         consultations.forEach( function(consultation) {
                             var link= arches.urls.resource+'/'+consultation["resourceinstanceid"];
                             results.push([
-                                $('<a></a>').attr("href",link).text(consultation['Name'])[0].outerHTML,
+                                $('<a></a>').attr("href",link).text(consultation['Management Activity Name'])[0].outerHTML,
                                 $('<a></a>').attr("href",link).text(consultation['Consultation Type'])[0].outerHTML,
                                 $('<a></a>').attr("href",link).text(consultation['Target Date'])[0].outerHTML,
-                                $('<a></a>').attr("href",link).text(consultation['Casework Officer'])[0].outerHTML,
-                                $('<a></a>').attr("href",link).html(consultation['Proposal'])[0].outerHTML
+                                $('<a></a>').attr("href",link).text(consultation['Agent'])[0].outerHTML,
+                                $('<a></a>').attr("href",link).html(consultation['Scope of Work Description'])[0].outerHTML
                             ]);
                         });
                         return results;
