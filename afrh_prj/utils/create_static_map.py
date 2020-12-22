c1de83ab35eea15637c0cca05a41fe6c079e34be
Cyrus Hiatt <chiatt@fargeo.com>
2020-12-22 15:03:30 -0800

    Adds parameters for line widths

--- afrh_prj/utils/create_static_map.py
+++ afrh_prj/utils/create_static_map.py
@@ -3,11 +3,23 @@ import os
 
 
 class StaticMapCreator(object):
-    def __init__(self, poly_fill = "#00FF003F", line_color = "#00FF00", outline_color = "#ff0000", marker_color= "#00FF00"):
+    def __init__(self, 
+            poly_fill="#FF00003F", 
+            line_color="#FF0000", 
+            outline_color="#A60000", 
+            marker_color="#FF0000",
+            line_width=2,
+            outline_width=2,
+            point_size=10
+            ):
+        self.point_size = point_size
+        self.line_width = line_width
+        self.outline_width = outline_width
         self.poly_fill = poly_fill
         self.line_color = line_color
         self.outline_color = outline_color
         self.marker_color = marker_color
+
         self.tile_provider_OSMH = staticmaps.TileProvider(
             "humanitarian",
             url_pattern="https://$s.tile.openstreetmap.fr/hot/$z/$x/$y.png",
@@ -15,6 +27,7 @@ class StaticMapCreator(object):
             attribution="Maps & Data (C) OpenStreetMap.org contributors",
             max_zoom=19,
         )
+
         self.context = staticmaps.Context()
         # context.set_tile_provider(staticmaps.tile_provider_StamenTerrain)
         self.context.set_tile_provider(self.tile_provider_OSMH)
@@ -24,22 +37,22 @@ class StaticMapCreator(object):
         geom = staticmaps.Area(
             [staticmaps.create_latlng(lat, lng) for lng, lat in coords],
             fill_color=staticmaps.parse_color(self.poly_fill),
-            width=2,
+            width=self.outline_width,
             color=staticmaps.parse_color(self.outline_color),
         )
         self.context.add_object(geom)
 
-    def convert_from_line(self, coords, halo=3):
+    def convert_from_line(self, coords):
         geom = staticmaps.Line(
             [staticmaps.create_latlng(lat, lng) for lng, lat in coords],
             color=staticmaps.parse_color(self.outline_color),
-            width=halo
+            width=self.line_width + 2
         )
         self.context.add_object(geom)
         geom = staticmaps.Line(
             [staticmaps.create_latlng(lat, lng) for lng, lat in coords],
             color=staticmaps.parse_color(self.line_color),
-            width=2
+            width=self.line_width
         )
         self.context.add_object(geom)
 
@@ -47,7 +60,7 @@ class StaticMapCreator(object):
         geom = staticmaps.Marker(
             staticmaps.create_latlng(coords[1], coords[0]),
             color=staticmaps.parse_color(self.marker_color),
-            size=5
+            size=self.point_size
         )
         self.context.add_object(geom)
 
@@ -61,6 +74,6 @@ class StaticMapCreator(object):
                 self.convert_from_line(coords)
             if geom_type == "Point":
                 self.convert_from_point(coords)
-        self.context.set_zoom(17)
+        self.context.set_zoom(18)
         image = self.context.render_cairo(width, height)
         image.write_to_png(output)
