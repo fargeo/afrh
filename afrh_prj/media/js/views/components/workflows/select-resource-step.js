08281f75c33607434ac08444b6233aef12955426
Cyrus Hiatt <chiatt@fargeo.com>
2021-01-05 13:52:12 -0800

    Adds name of Management Activity after user returns to first step and disables it so user can't change it. re #4

--- afrh_prj/media/js/views/components/workflows/select-resource-step.js
+++ afrh_prj/media/js/views/components/workflows/select-resource-step.js
@@ -16,14 +16,36 @@ define([
         this.nameheading = params.nameheading;
         this.namelabel = params.namelabel;
         this.resValue.subscribe(function(val){
-            if (ko.unwrap(self.tile)) {
+            if (ko.unwrap(self.tile) && !self.tile().resourceinstance_id) {
                 self.tile().resourceinstance_id = ko.unwrap(val[0].resourceId);
             }
             params.resourceid(ko.unwrap(val[0].resourceId));
         }, this);
+        this.resourceDisplayName = ko.observable();
+        var lookupResourceInstanceData = function(resourceid) {
+            return window.fetch(arches.urls.search_results + "?id=" + resourceid)
+                .then(function(response){
+                    if(response.ok) {
+                        return response.json();
+                    }
+                })
+                .then(function(json) {
+                    return json["results"]["hits"]["hits"][0]["_source"];
+                })
+        };
 
         this.card.subscribe(function(val){ if(ko.unwrap(val) != undefined) { this.loading(false); } }, this);
-        this.tile.subscribe(function(val){ if(ko.unwrap(val) != undefined) { this.loading(false); } }, this);
+        this.tile.subscribe(function(val){ 
+            if(ko.unwrap(val) != undefined) { 
+                this.loading(false); 
+            } 
+            if (val.resourceinstance_id) {
+                var resourceInfo = lookupResourceInstanceData(val.resourceinstance_id);
+                resourceInfo.then(function(instance){
+                    self.resourceDisplayName(instance.displayname);
+                });
+            }
+        }, this);
         params.tile = self.tile;
 
         self.onSaveSuccess = function(tiles) {
